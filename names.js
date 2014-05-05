56b1e1be2bf85e0a467c800f8f621589f87cbc42
Casey Link <unnamedrambler@gmail.com>
2014-05-05 16:05:23 +0200

    first version

---
+++ names.js
@@ -0,0 +1,120 @@
+// Name Data from Firefly RPG Corebook
+// Get yours here: http://www.margaretweis.com/index.php/shop#!/~/product/id=29762279
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+gen_data['name-male-oldwest'] = ['{ow_male} {ow_family}'];
+gen_data['name-female-oldwest'] = ['{ow_female} {ow_family}'];
+gen_data['ow_male'] = ["Alva", "Anton", "Billy", "Cecil", "Clayton", "Cleveland", "Curtis", "Edmond", "Eli", "Ellis", "Frank", "George", "Harley", "Hubert", "Irving", "Jesse", "Ollie", "Robert", "Rudolph", "Thomas", "Vernon", "Wiley", "Wyatt"];
+gen_data['ow_female'] = ["Adeline", "Agnes", "Annie", "Bess", "Beth", "Birdie", "Cornelia", "Dollie", "Edith", "Electra", "Eliza", "Flossie", "Harriett", "Irene", "Jane", "Lizzie", "Loretta", "Mary", "Myra", "Olga", "Rhoda", "Roxie", "Sally", ];
+gen_data['ow_family'] = ["Archer", "Bailey", "Carson", "Clay", "Dawson", "Edwards", "Fisher", "Floyd", "Gaines", "Galveston", "Hamilton", "Hays", "Howard", "Jefferson", "King", "Lee", "Montaghue", "Oldham", "Polk", "Rains", "Shackelford", "Upton", "Winkler"];
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+gen_data['name-male-chinese'] = ['{name_male_chinese} {name_family_chinese}'];
+gen_data['name-female-chinese'] = ['{name_female_chinese} {name_family_chinese}'];
+gen_data['name_family_chinese'] = ["Gao", "Guo", "Hu", "Huang", "Jiang", "Li", "Lin", "Liu", "Ma", "Pan", "Su", "Sun", "Tang", "Wang", "Wei", "Wu", "Xie", "Yang", "Yu", "Zhang", "Zhao", "Zheng", "Zhou", ];
+gen_data['name_female_chinese'] = ["Ai (love)", "An (peace)", "Bai (white)", "Chun (Spring)", "Fang (fragrant)", "He (lotus)", "Hua (flower)", "Jia (lovely and good)", "Jin  (golden)", "Lan (orchid)", "Li (beautiful)", "Ling (exquisite)", "Mei (pretty)", "Qi (finest jade)", "Qiao (clever)", "Qin (stringed instrument)", "Qing (clean; pure)", "Wan (gracious)", "Xin (heart)", "Xing (start)", "Xiu (elegant)", "Yi (happy)", "Yue (moon)", ];
+gen_data['name_male_chinese'] = ["Bao (precious)", "Bai (hundred)", "Biao (young tiger)", "Cheng (to become; success)", "Da (big)", "De (moral)", "Fei (flyin’; swift)", "Gong (merit)", "Hong (great; grand)", "Jian (healthy)", "Lei (thunder)", "Li (strength)", "Ming (brightness)", "Ping (calm; even)", "Rong (glory)", "Qiang (strong)", "Shen (god; divine)", "Sheng (sage; saintly)", "Tai (highest; greatest)", "Tian (sky; celestial)", "Xiong (masculine)", "Yang (sun)", "Zihao (son heroic)", ];
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+gen_data['name-male-core'] = ['{name_male_core} {name_family_core}'];
+gen_data['name-female-core'] = ['{name_female_core} {name_family_core}'];
+gen_data['name_male_core'] = ["Winston", "Wilson", "Ward", "Warren", "Thomas", "Spencer", "Sinclair", "Montgomery", "Phillip", "Howard", "Mitchell", "Nicholas", "Percy", "Reginald", "Francis", "Gregory", "Nathaniel", "Quentin", "Atticus", "Cuthbert", "Crispin", "Hugo", "Conrad", ];
+gen_data['name_female_core'] = ["Kendall ", "Genevieve ", "Beatrice ", "Yvonne ", "Elizabeth ", "Meredith ", "Jacqueline ", "Lucille ", "Victoria ", "Arabella ", "Camilla ", "Cordelia ", "Ophelia ", "Augusta ", "Octavia ", "Belinda ", "Helena ", "Florence ", "Sophia ", "Rebecca ", "Carlotta ", "Phillipa ", "Rowena ", ];
+gen_data['name_family_core'] = ["Bates ", "Yeatman ", "Wynn ", "Wright ", "Woodruff ", "Barker ", "Thornton ", "Underwood ", "Tuttle ", "Townsley ", "Townshend ", "Stuart ", "Tate ", "Sutton ", "King ", "Stanford ", "Rutherford ", "Sexton ", "Richmond ", "Quinn ", "Pearson ", "Powell ", "Andersen ", ];
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+gen_data['name-male-redsun'] = ['{name_male_redsun} {name_family_redsun}'];
+gen_data['name-female-redsun'] = ['{name_female_redsun} {name_family_redsun}'];
+gen_data['name_male_redsun'] = ["Sherman", "Russel", "Seymour", "Ambrose", "Billy", "Cole", "Cassidy", "Clarence", "Elijah", "Gus", "Hank", "Jesse", "Luke", "Owen", "Sam", "Travis", "Zeke", "Bai", "Chang", "Chao", "Chen", "Hai", "Han", ]
+gen_data['name_female_redsun'] = ["Chow", "Chun", "Fung", "Hua", "Jun", "Lee", "Lian", "Mei", "Shun", "Ting", "Wen", "Yan", "Yu", "Abigail", "Ada", "Charlotte", "Daisy", "Ellie", "Hannah", "Hattie", "Judith", "Maybelle", "Nelly", ]
+gen_data['name_family_redsun'] = ["Sullivan", "Skinner", "Richards", "Puckett", "Nicholson", "Mucklebreed", "Michaels", "McCoy", "McGee", "McDonnell", "Lewis", "Kaine", "Smith", "Brown", "Lee", "Wilson", "Taylor", "Davis", "Lee", "Harris", "Clark", "Young", "Adams", ]
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+gen_data['name-male-georgia'] = ['{name_male_georgia} {name_family_georgia}'];
+gen_data['name-female-georgia'] = ['{name_female_georgia} {name_family_georgia}'];
+gen_data['name_male_georgia'] = ["Earnest", "Felix", "Lucky", "Clem", "Mick", "Sully", "Cal", "Judd", "Ned", "Theo", "Charlie", "Gus", "Jeb", "Ray", "Hal", "Nat", "Ty", "Will", "Sam", "Kit", "Jack", "Andy", "Chet", ]
+gen_data['name_female_georgia'] = ["Chastity", "Charity", "Clemency", "Mercy", "Temperance", "Constance", "Hope", "Grace", "Faith", "Honor", "Cornelia", "Sissy", "Maggie", "Abby", "Frankie", "Poppy", "Penny", "Tessie", "Nellie", "Tillie", "Sadie", "Lulu", "Aggie", ]
+gen_data['name_family_georgia'] = ["Oakley", "Masterson", "Starr", "Courtright", "Doolin", "Pickett", "Bart", "Ketchum", "Dalton", "Ford", "Cody", "Cannary", "Madsen", "Allison", "Younger", "Douglas", "Holliday", "Borne", "Horner", "McJunkin", "Clanton", "Ware", "Hardin", ]
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+gen_data['name-male-bluesun'] = ['{name_male_bluesun} {name_family_bluesun}'];
+gen_data['name-female-bluesun'] = ['{name_female_bluesun} {name_family_bluesun}'];
+gen_data['name_male_bluesun'] = ["Mark", "Paul", "Robert", "William", "Donald", "George", "Kenneth", "Steven", "Brian", "Edward", "Joseph", "Charles", "Richard", "David", "Christopher", "Ronald", "James", "John", "Anthony", "Thomas", "Michael", "Jason", "Tyler", ]
+gen_data['name_female_bluesun'] = ["Patricia", "Linda", "Barbara", "Jennifer", "Susan", "Margaret", "Dorothy", "Lisa", "Nancy", "Karen", "Betty", "Sandra", "Donna", "Carol", "Ruth", "Sharon", "Michelle", "Laura", "Kimberly", "Deborah", "Helen", "Ava", "Natalie", ]
+gen_data['name_family_bluesun'] = ["Archer", "Bond", "Cartwright", "Dyer", "Fletcher", "Harper", "Horner", "Knight", "Mason", "Piper", "Planter", "Porter", "Potter", "Sawyer", "Seaman", "Shoemaker", "Spencer", "Skinner", "Tanner", "Taylor", "Thatcher", "Wainwright", "Weaver", ]
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+gen_data['name-male-kalidasa'] = ['{name_male_kalidasa} {name_family_kalidasa}'];
+gen_data['name-female-kalidasa'] = ['{name_female_kalidasa} {name_family_kalidasa}'];
+gen_data['name_male_kalidasa'] = ["Hassan", "Elias", "Adolfo", "Noel", "Lucas", "Viktor", "Malik", "Davu", "Marcus", "Conley", "Juan", "Bedros", "Liam", "Jayden", "Emeka", "Wataru", "Cooper", "Lorenzo", "Noah", "Ayo", "Mateo", "Bunmei", "Isaiah", ]
+gen_data['name_female_kalidasa'] = ["Fatima", "Maryam", "Jada", "Zahra", "Iwalani", "Magda", "Kirabo", "Sanaa", "Remika", "Gisela", "Lena", "Abiba", "Nomusa", "Gabrielle", "Anya", "Maria", "Liliana", "Chloe", "Tahlia", "Breena", "Cyrah", "Olga", "Elyse", ]
+gen_data['name_family_kalidasa'] = ["Vargas", "Dupont", "Novak", "Garcia", "Nielsen", "Ivanov", "Petrovavich", "Singh", "Isa", "Karlsson", "Antoniak", "Hashimoto", "Roux", "Giovanni", "Van der Meer", "Ndiaye", "O’Sullivan", "Schumacher", "Kawaguchi", "Gallo", "Costa", "Okoro", "De Wit", ]
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+var options = [{
+    "Male Names": [{
+        "id": "name-male-core",
+        "name": "Core Worlds"
+    }, {
+        "id": "name-male-redsun",
+        "name": "Border: Red Sun System"
+    }, {
+        "id": "name-male-georgia",
+        "name": "Border: Georgia System"
+    }, {
+        "id": "name-male-bluesun",
+        "name": "Rim: Blue Sun System"
+    }, {
+        "id": "name-male-kalidasa",
+        "name": "Rim: Kalidasa System"
+    }, {
+        "id": "name-male-chinese",
+        "name": "Chinese"
+    }, {
+        "id": "name-male-oldwest",
+        "name": "Old West"
+    }]
+}, {
+    "Female Names": [{
+        "id": "name-female-core",
+        "name": "Core Worlds"
+    }, {
+        "id": "name-female-redsun",
+        "name": "Border: Red Sun System"
+    }, {
+        "id": "name-female-georgia",
+        "name": "Border: Georgia System"
+    }, {
+        "id": "name-female-bluesun",
+        "name": "Rim: Blue Sun System"
+    }, {
+        "id": "name-female-kalidasa",
+        "name": "Rim: Kalidasa System"
+    }, {
+        "id": "name-female-chinese",
+        "name": "Chinese"
+    }, {
+        "id": "name-female-oldwest",
+        "name": "Old West"
+    }]
+}];
+$(function() {
+    var $select = $("#gentype");
+    $.each(options, function(i, optgroups) {
+        $.each(optgroups, function(groupName, options) {
+            var $optgroup = $("<optgroup>", {
+                label: groupName
+            });
+            $optgroup.appendTo($select);
+            $.each(options, function(j, option) {
+                var $option = $("<option>", {
+                    text: option.name,
+                    value: option.id
+                });
+                $option.appendTo($optgroup);
+            });
+        });
+    });
+    $('#clear').click(function() {
+        $('#result').empty();
+    });
+    $('#generate').click(function() {
+        var gentype = $("#gentype").val();
+        $('#result').prepend(generate_text(gentype) + "<br/>");
+    });
+    $(document).ready(function() {
+        $('#generate').click();
+    });
+});
