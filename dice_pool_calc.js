d91cb3941d00ceb24236fd26581457e5345c64ae
Casey Link <unnamedrambler@gmail.com>
2014-05-20 16:34:50 +0200

    add dice probability calculator

---
+++ dice_pool_calc.js
@@ -0,0 +1,225 @@
+var natsort = function(a,b) {
+  if (isNaN(a) || isNaN(b)) {
+    return a > b ? 1 : -1;
+  }
+  return a - b;
+}
+var flatten = function(l) {
+        return l.reduce(function(m, x) {
+            if (x.length) m = m.concat(x);
+            return m;
+        }, []);
+    };
+var first = function(list) {
+        return list[0];
+    };
+var rest = function(list) {
+        return list.slice(1);
+    };
+var sum = function(list) {
+        return list.reduce(function(total, value) {
+            return total == 0 ? value : total.map(function(val, index) {
+                return val + value[index];
+            });
+        }, 0);
+    };
+var enumerate = function(lists) {
+        if (!lists || lists.length == 0) return [];
+        if (lists.length == 1) return first(lists);
+        return flatten(first(lists).map(function(value) {
+            return enumerate(rest(lists)).map(function(value2) {
+                return [value].concat(value2);
+            });
+        }));
+    };
+var cortex2 = function(rolls) {
+        return rolls.sort(natsort).reverse().map(function(roll) {
+            return roll == 1 ? 0 : roll; // turn the ones into zeros, so we don’t accidentally add them to the total
+        }).reduce(function(rolls, roll) {
+            return [rolls[0] + (rolls[2] < 2 ? roll : 0), rolls[1] + (roll == 0 ? 1 : 0), rolls[2] + 1];
+        }, [0, 0, 0]);
+};
+var cortexenum = function(dice) {
+    var one = sum(enumerate(dice).map(cortex2))
+    var two = one.map(function(value) {
+        return value / dice.reduce(function(total, die) {
+            return total * die.length
+        }, 1);
+    });
+    return two;
+};
+
+//var go = cortexenum([[1,2,3,4],[1,2,3,4]]);
+
+function DieRoll(sides, result) {
+    this.sides = sides;
+    this.result = result;
+    this.randomize =  function() {
+        this.result = Math.ceil(Math.random() * this.sides);
+    };
+    if( this.result < 0 ) this.randomize();
+};
+
+function DiceSort(A, B) {
+    if( A.result > B.result)
+        return -1;
+    else if (A.result < B.result)
+        return 1;
+    else {
+        if( A.sides < B.sides)
+            return -1;
+        else if( A.sides > B.sides)
+            return 1;
+        return 0;
+    }
+};
+
+function DicePool() {
+    this.pool = Array();
+    this.addDie = function(sides) {
+        var die = new DieRoll(sides, -1);
+        this.pool.push(die);
+    };
+    this.addDieRoll = function(die) {
+        this.pool.push(die);
+    };
+    this.getResult = function() {
+        this.sort();
+        if(this.pool.length == 0) {
+            return 0;
+        } else if (this.pool.length == 1) {
+            return this.pool[0].result == 1 ? 0 : this.pool[0].result;
+        } else {
+            var result = 0;
+            if(this.pool[0].result != 1)
+                result += this.pool[0].result;
+            if(this.pool[1].result != 1)
+                result += this.pool[1].result;
+            return result;
+        }
+    };
+    this.getJinxes = function() {
+        var jinxes = 0;
+        for(var i = 0; i < this.pool.length; i++) {
+            if( this.pool[i].result == 1 )
+                jinxes++;
+        }
+        return jinxes;
+    };
+    this.sort = function() {
+        this.pool = this.pool.sort(DiceSort);
+    };
+    this.clone = function() {
+        var cloned = new DicePool();
+        for( var i = 0; i < this.pool.length; i++) {
+            var die = this.pool[i];
+            cloned.pool.push(new DieRoll(die.sides, die.result));
+        }
+        return cloned;
+    };
+}
+function CortexDice(pool) {
+    this.dice = pool;
+    //dice: int[]
+    this.getMaximum = function() {
+
+        if( this.dice.length == 1)
+            return this.dice[0];
+        var l = this.dice.length;
+        return this.dice[l-1]+this.dice[l-2];
+    };
+    this.getResultsTable = function() {
+        var possibles = Array();
+        for( var i = 0; i < this.dice.length; i++) {
+            var sides = this.dice[i];
+            var cloned = Array();
+            if( possibles.length == 0 ) {
+                for ( var j = 1; j <= sides; j++ ) {
+                    var pool = new DicePool();
+                    pool.addDieRoll(new DieRoll(sides, j));
+                    cloned.push(pool);
+                }
+            } else {
+                for ( var j = 1; j <= sides; j++ ) {
+                    for ( var k = 0; k < possibles.length; k++ ) {
+                        var oldPool = possibles[k];
+                        var newPool = oldPool.clone();
+                        newPool.addDieRoll(new DieRoll(sides, j));
+                        cloned.push(newPool);
+                    }
+                }
+            }
+            possibles = cloned;
+        }
+        return possibles;
+    };
+    this.results = this.getResultsTable();
+
+    this.getAverageResult = function() {
+        var possibilities = this.results.length;
+        var total = 0;
+        var foo = [];
+        for(var i=0; i < this.results.length; i++) {
+            total += this.results[i].getResult();
+            foo.push(this.results[i].getResult());
+        }
+        var mean = total/possibilities;
+        return mean;
+    };
+    this.getChanceOf = function(value) {
+        var possibilities = this.results.length;
+        var total = 0;
+        for(var i=0; i < this.results.length; i++) {
+            if(this.results[i].getResult() == value) {
+                total++;}
+        };
+        var proba = total/possibilities;
+        return proba;
+
+    };
+    this.getAverageJinxes = function() {
+        var possibilities = this.results.length;
+        var total = 0;
+        for(var i=0; i < this.results.length; i++) {
+            total += this.results[i].getJinxes();
+        }
+        var mean = total/possibilities;
+        return mean;
+    };
+    this.getJinxChance = function() {
+        var possibilities = this.results.length;
+        var total = 0;
+        for(var i=0; i < this.results.length; i++) {
+            if(this.results[i].getJinxes() > 0 )
+                total++;
+        }
+        var probability = total/possibilities;
+        return probability;
+    };
+
+}
+
+self.addEventListener('message', function(e) {
+    console.log("WORKER GOT");
+    var pool = e.data.pool;
+
+    var c = new CortexDice(pool);
+    var mean = c.getAverageResult();
+    var mean_jinx= c.getAverageJinxes();
+    var chance_jinx = c.getJinxChance();
+    var max = c.getMaximum();
+    var freq = { 'max': max, 'start':2 };
+    for( var i=2;i<=max;i++) {
+        var prob = c.getChanceOf(i);
+        freq[i] = prob;
+    }
+    var result = { 'mean': mean,
+                   'mean_jinx': mean_jinx,
+                   'chance_jinx': chance_jinx,
+                   'max': max,
+                   'frequency_dist': freq,
+    };
+    postMessage({'result': result});
+
+
+}, false);
---
+++ probability.js
@@ -0,0 +1,100 @@
+var dX = function(d,n) {
+    if( isNaN(n) ) n = 0;
+    if ( n == 0 ) return [];
+    var all = Array(n+1).join(1).split('').map(function(){return d;})
+    return all;
+};
+
+/* Check if Web Workers are supported */
+function getWebWorkerSupport() {
+    return (typeof(Worker) !== "undefined") ? true:false;
+}
+
+function googleCallback() {
+    $('#dice_prob :input').prop('disabled', false);
+}
+
+function render(result) {
+    var mean = result.mean
+    var mean_jinx= result.mean_jinx;
+    var chance_jinx = result.chance_jinx*100;
+    var max = result.max;
+
+    $("#mean-roll").text(mean.toFixed(2));
+    $("#mean-jinx").text(mean_jinx.toFixed(2));
+    $("#chance-jinx").text(chance_jinx.toFixed(2)+"%");
+    $("#max-value").text(max);
+
+    var values = [];
+    for( var i = result.frequency_dist.start; i<=result.frequency_dist.max; i++) {
+        var prob = result.frequency_dist[i]*100;
+        var val = i.toString();
+        var tip = "<div style='width:100%; padding: 5px'> "+val+": has a <strong>"+prob.toFixed(2)+"%</strong> chance to roll</div>";
+        values.push([i.toFixed(0), prob, tip]);
+    }
+
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Roll');
+    data.addColumn('number', 'Percentage');
+    data.addColumn({type: 'string', role: 'tooltip','p': {'html': true}});
+    data.addRows(values);
+    var view =  new google.visualization.DataView(data);
+
+    var options = {
+        legend: { position: 'none' },
+        width: 550,
+        height: 400,
+        chartArea: {'width': '82%', 'height': '85%'},
+        tooltip: {isHtml: true},
+        vAxis: {'title': 'Pool Value'},
+        hAxis: {'title': 'Chance (%)'}
+    };
+
+    $("#graph_note").toggleClass('hide');
+    var chart = new google.visualization.BarChart(document.getElementById('chart'));
+    chart.draw(data, options);
+    toggleSpinner();
+};
+
+function toggleSpinner() {
+    $("#chart_container").toggleClass('hide');
+    $("#results").toggleClass('hide');
+    $("#spinner").toggleClass('hide');
+
+}
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(googleCallback);
+
+var worker = new Worker("dice_pool_calc.js");
+
+worker.addEventListener("message", function (e) {
+    render(e.data.result);
+}, false);
+
+$(function() {
+    if ( !getWebWorkerSupport() )
+        $('#oldbrowser').removeClass('hide');
+
+    $('#tt_mean').tooltip();
+    $('#tt_jinxmean').tooltip();
+    $('#tt_jinxchance').tooltip();
+    $('#tt_max').tooltip();
+    $('#but_reset').click(function() {
+        $('#dice_prob :input').val("");
+    });
+    $('#dice_prob :input').change(function(){
+
+        var d4 = Number($("#d4").val());
+        var d6 = Number($("#d6").val());
+        var d8 = Number($("#d8").val());
+        var d10 = Number($("#d10").val());
+        var d12 = Number($("#d12").val());
+
+        var pool = [dX(4,d4), dX(6,d6), dX(8,d8), dX(10,d10), dX(12,d12)];
+        pool = pool.filter(function(n){ return n.length != 0 }).reduce(function(a,b) { return a.concat(b)});
+        if (pool.length < 2 ) return;
+
+        worker.postMessage({'pool':pool});
+        toggleSpinner();
+    });
+});
